f51ecbe4d3455020af7699fff53361adcb62dfe6
kirabiyakasa <morning_star@kirabiyakasa.moe>
2021-02-24 05:16:54 -0500

    Add BoardBuilder class and update board

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,140 @@
-class board
+require './lib/board_builder.rb'
+
+require 'pry'
+
+class Board
+  attr_reader :p1, :p2, :spaces
+
+  def initialize(player1, player2)
+    @p1 = player1
+    @p2 = player2
+    board_builder = BoardBuilder.new
+    @spaces = board_builder.build_board
+  end
+
+  def move_piece(interface, player)
+    start_coords = select_piece(interface, player)
+    end_coords = false
+    while end_coords == false || end_coords == 'cancel'
+      end_coords = select_destination(interface, player, start_coords)
+      if end_coords == 'cancel'
+        start_coords = select_piece(interface, player)
+      end
+    end
+    binding.pry
+    # ---------------------------------
+    move_resolved = resolve_move(start_coords, end_coords)
+    return move_resolved
+    # get selected piece, move to space.
+    # proceed to next player's turn
+    # ---------------------------------
+    # get opposing player in this method
+    # pass start coords and end coords?
+  end
+
+  private
+
+  def get_coordinates(interface)
+    interface.ask_for_file
+    file = gets.chomp
+    until ('a'..'h').include?(file.downcase) || file == 'y'
+      interface.show_invalid_input
+      file = gets.chomp
+    end
+    if file == 'y'
+      puts 'Input canceled'
+      return 'cancel'
+    end
+    file = convert_file(file.downcase).to_i
+  
+    interface.ask_for_rank
+    rank = gets.chomp
+    until ('1'..'8').include?(rank)
+      interface.show_invalid_input
+      rank = gets.chomp
+    end
+    rank = rank.to_i - 1
+    return [rank, file]
+  end
+  # y key is cancel
+
+  def get_space(coords)
+    rank = coords[0]
+    file = coords[1]
+    space = @spaces[rank][file]
+    return space
+  end
+
+  def select_piece(interface, player)
+    interface.show_piece_select
+    start_coords = get_coordinates(interface)
+    piece = get_space(start_coords)
+    while piece == ' ' || player.color != piece.color
+      if start_coords == 'cancel'
+        puts 'Input canceled. Select a new piece to move.'
+      else
+        interface.show_invalid_selection(start_coords)
+      end
+      start_coords = get_coordinates(interface)
+      piece = get_space(start_coords)
+    end
+    return start_coords
+  end
+
+  def select_destination(interface, player, start_coords)
+    interface.show_destination_select
+    end_coords = get_coordinates(interface)
+    space = get_space(end_coords)
+
+    move_legality = false
+    while move_legality == false || end_coords == 'cancel'
+      if end_coords == 'cancel'
+        return 'cancel'
+      else
+        move_legality = legal_move?(start_coords, end_coords)
+      end
+  
+      if move_legality == false
+        interface.show_invalid_destination
+        end_coords = get_coordinates(interface)
+      else
+        space = get_space(end_coords)
+      end
+    end
+    return end_coords
+  end
+
+  def legal_move?(start_coords, end_coords)
+    rank = start_coords[0]
+    file = start_coords[1]
+    start_piece = @spaces[rank][file]
+    
+    legality = start_piece.check_legal_move(start_coords, end_coords, @spaces)
+    return legality
+  end
+
+  def resolve_move(start_coords, end_coords)
+    start_rank = start_coords[0]
+    start_file = start_coords[1]
+
+    end_rank = end_coords[0]
+    end_file = end_coords[1]
+
+    piece = @spaces[start_rank][start_file]
+    @spaces[end_rank][end_file] = piece
+    @spaces[start_rank][start_file] = ' '
+    return true
+  end
+  # work here
+
+  def convert_file(file)
+    file_letters = ("a".."h").to_a
+    if file_letters.include?(file)
+      file = file_letters.find_index(file).to_s
+      return file
+    else
+      return nil
+    end
+  end
+
 end
---
+++ lib/board_builder.rb
@@ -0,0 +1,69 @@
+require './lib/pieces/pieces_helper'
+
+require './lib/pieces/bishop.rb'
+require './lib/pieces/king.rb'
+require './lib/pieces/knight.rb'
+require './lib/pieces/pawn.rb'
+require './lib/pieces/queen.rb'
+require './lib/pieces/rook.rb'
+
+class BoardBuilder
+
+  def build_board()
+    white_pieces = get_pieces('white')
+    black_pieces = get_pieces('black')
+
+    black_row1 = get_initial_row(black_pieces)
+    white_row1 = get_initial_row(white_pieces)
+    black_row2 = black_pieces[:pawn]
+    white_row2 = white_pieces[:pawn]
+  
+    spaces =
+    {
+      7 => black_row1,
+      6 => black_row2,
+      5 => [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      4 => [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      3 => [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      2 => [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      1 => white_row2,
+      0 => white_row1
+    }
+    return spaces
+  end
+
+  private
+
+  def get_pieces(color)
+    pieces = {}
+    pieces[:king] = King.new(color)
+    pieces[:queen] = Queen.new(color)
+    pieces[:bishop] = []
+    pieces[:knight] = []
+    pieces[:rook] = []
+    pieces[:pawn] = []
+    2.times do
+      pieces[:bishop] << Bishop.new(color)
+      pieces[:knight] << Knight.new(color)
+      pieces[:rook] << Rook.new(color)
+    end
+    8.times do
+      pieces[:pawn] << Pawn.new(color)
+    end
+    return pieces
+  end
+  
+  def get_initial_row(pieces)
+    initial_row = []
+    initial_row << pieces[:queen]
+    initial_row << pieces[:king]
+    initial_row_pieces = [pieces[:bishop], pieces[:knight], pieces[:rook]]
+
+    initial_row_pieces.each do |piece_set|
+      initial_row.unshift(piece_set[0])
+      initial_row.push(piece_set[1])
+    end
+    return initial_row
+  end
+
+end
