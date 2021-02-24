ffc0f109e1d54d86809d6befdddb5c5f7a6dcee5
kirabiyakasa <morning_star@kirabiyakasa.moe>
2021-02-24 05:12:33 -0500

    Add GameLogic class

---
+++ lib/game_logic.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class GameLogic
+
+  def initialize(interface, board)
+    @interface = interface
+    @board = board
+  end
+
+  def play_game()
+    move_white()
+  end
+
+  private
+
+  def move_white()
+    @board.p1.color == 'white' ? player = @board.p1 : player = @board.p2
+    @interface.show_board(@board.spaces, @board.p1, @board.p2)
+    move_resolved = false
+    until move_resolved == true
+      move_resolved = @board.move_piece(@interface, player)
+    end
+    binding.pry
+    move_black()
+  end
+
+  def move_black()
+    @board.p1.color == 'black' ? player = @board.p1 : player = @board.p2
+    @interface.show_board(@board.spaces, @board.p1, @board.p2)
+    move_resolved = false
+    until move_resolved == true
+      move_resolved = @board.move_piece(@interface, player)
+    end
+    binding.pry
+    move_white()
+  end
+
+end
