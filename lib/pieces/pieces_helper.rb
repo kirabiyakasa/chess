30295953bbe0d5657329d8b6666c8c90338aefe2
kirabiyakasa <morning_star@kirabiyakasa.moe>
2021-02-24 05:22:07 -0500

    Add pieces helper

---
+++ lib/pieces/pieces_helper.rb
@@ -0,0 +1,19 @@
+module PiecesHelper
+
+  def check_legal_move(start_coords, end_coords, spaces)
+    rank = end_coords[0]
+    file = end_coords[1]
+    space = spaces[rank][file]
+
+    move_validity = validate_move(start_coords, end_coords, spaces, space)
+    return move_validity
+  end
+
+  def capture(spaces, capture_coords)
+    rank = capture_coords[0]
+    file = capture_coords[1]
+    spaces[rank].slice!(file)
+    spaces[rank].insert(file, ' ')
+  end
+
+end
