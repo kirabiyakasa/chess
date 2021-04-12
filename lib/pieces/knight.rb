7a0865a0863a76c656be1af1cf7e80ddc4b4ed73
kirabiyakasa <morning_star@kirabiyakasa.moe>
2021-04-12 14:40:07 -0400

    Add move validations for Knight.

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,5 +1,7 @@
 class Knight
-  attr_reader :en_passant, :color, :icon
+  include PiecesHelper
+
+  attr_reader :color, :icon
 
   def initialize(color)
     @color = color
@@ -9,6 +11,33 @@ class Knight
       [-2, 1], [-1, 2], [2, 1], [1, 2],
       [-2, -1], [-1, -2], [2, -1], [1, -2]
     ]
+  end
+
+  private
+
+  def validate_move(start_coords, end_coords, spaces, space)
+    rank = end_coords[0] - start_coords[0]
+    file = end_coords[1] - start_coords[1]
+    coord_change = [rank, file]
+    capture_coords = [end_coords[0], end_coords[1]]
+
+    if knight_movement?(start_coords, coord_change, end_coords, spaces)
+      capture(spaces, capture_coords)
+      return true
+    end
+    return false
+  end
+
+  def knight_movement?(start_coords, coord_change, end_coords, spaces)
+    destination = spaces[end_coords[0]][end_coords[1]]
+    unless destination == ' '
+      return false if destination.color == @color
+    end
+
+    if @move_list.include?(coord_change)
+      return true
+    end
+    return false
   end
 
   def get_icon()
