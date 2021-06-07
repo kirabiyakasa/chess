debbfe05c9c8301f3c1eef82d06d6be5c589f279
kirabiyakasa <morning_star@kirabiyakasa.moe>
2021-06-06 23:58:22 -0400

    Change where colors and names are defined

--- lib/player.rb
+++ lib/player.rb
@@ -1,33 +1,9 @@
 class Player
-  attr_reader :name, :color, :pieces
+  attr_reader :name, :color
 
-  def initialize(colors, player_num)
-    @name = get_name(player_num)
-    @color = choose_color(colors)
-  end
-
-  private
-
-  def get_name(player_num)
-    puts "\nEnter a name for #{player_num}."
-    name = gets.chomp
-    return name
-  end
-
-  def choose_color(colors)
-    if colors.length == 2
-      puts "\nSelect a Color for #{@name}."
-      puts "\n1) Black    2) White"
-      answer = gets.chomp
-      until answer == '1' || answer == '2'
-        puts 'Please Enter a valid input.'
-        answer = gets.chomp
-      end
-      answer == '1' ? color = colors.shift : color = colors.pop
-    else
-      color = colors.pop
-    end
-    return color
+  def initialize(color, name)
+    @color = color
+    @name = name
   end
 
 end
--- main.rb
+++ main.rb
@@ -3,25 +3,53 @@ require './lib/board.rb'
 require './lib/player.rb'
 require './lib/game_logic.rb'
 
+def get_name(player_num)
+  puts "\nEnter a name for #{player_num}."
+  name = gets.chomp
+  return name
+end
+
+def choose_color(colors)
+  if colors.length == 2
+    puts "\nSelect a Color for #{@name}."
+    puts "\n1) Black    2) White"
+    answer = gets.chomp
+    until answer == '1' || answer == '2'
+      puts 'Please Enter a valid input.'
+      answer = gets.chomp
+    end
+    answer == '1' ? color = colors.shift : color = colors.pop
+  else
+    color = colors.pop
+  end
+  return color
+end
+
+def setup_players(colors)
+  p1_name = get_name('player 1')
+  p2_name = get_name('player 2')
+
+  p1_color = choose_color(colors)
+  p2_color - choose_color(colors)
+
+  player1 = Player.new(p1_color, p1_name)
+  player2 = Player.new(p2_color, p2_name)
+
+  return [player1, player2]
+end
 
 def start_game()
   interface = Interface.new
   interface.show_rules
   colors = ['white', 'black']
-  player1 = Player.new(colors, 'player 1')
-  player2 = Player.new(colors, 'player 2')
-  board = Board.new(player1, player2)
+  players = setup_players(colors)
+  board = Board.new(players[0], players[1])
   game = GameLogic.new(interface, board)
   game.play_game
 end
 start_game()
 
-# check if move is valid
 # check if king is in check and disallow all other moves
-# remove piece if captured
-# resolve move
-# switch to black, and do the same
-# alternate black and white
 
 # allow for castling
 # allow for piece recovery if pawn reaches other side of the board
